51970f51e1ad7cf26d82e8b396a264fa5a846d1c
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-22 21:27:07 +0600

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      '@typescript-eslint': true,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
