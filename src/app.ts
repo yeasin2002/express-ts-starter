73088fd9030671a9c279bebbc7a9c4d9e15d191c
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-22 21:09:43 +0600

    initial basic setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import { PORT } from "./utils/exportEnv";
+
+const app = express();
+
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
+});
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+  });
