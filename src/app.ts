9c2ca7f7a4254b2d703a3670b8097f6471744cda
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-22 21:22:23 +0600

    added formatter and global middleware and scripts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Request, Response } from "express";
-import { PORT } from "./utils/exportEnv";
+import express, { Request, Response } from 'express';
+import { PORT } from './utils/exportEnv';
 
 const app = express();
 
-
-app.get("/", (request: Request, response: Response) => {
-  response.status(200).send("Hello World");
+app.get('/', (request: Request, response: Response) => {
+  response.status(200).send('Hello Yeasin');
 });
 
 app
   .listen(PORT, () => {
-    console.log("Server running at PORT: ", PORT);
+    console.log('Server running at PORT: ', PORT);
   })
-  .on("error", (error) => {
+  .on('error', (error) => {
     // gracefully handle error
     throw new Error(error.message);
   });
