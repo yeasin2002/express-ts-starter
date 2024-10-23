a7f3d4e573411314806731e8418c73f5d49b43de
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-23 17:30:08 +0600

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    plugins: {
-      '@typescript-eslint': true,
-    },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-member-accessibility': 'error',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      'no-undef': 'off',
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+);
