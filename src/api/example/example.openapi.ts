e7e010365255c15b31bbe79f12d5f0b32de430cd
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-31 10:30:58 +0600

    chore: added example module

---
+++ src/api/example/example.openapi.ts
@@ -0,0 +1,51 @@
+
+import { registry } from "@/lib/openapi";
+import { mediaTypeFormat, openAPITags } from "@/common/constants";
+
+// registry.register("example", exampleSchema);
+registry.registerPath({
+  method: "post",
+  path: "/api/example", // use openAPITags basepath - Example: openAPITags.category.basepath
+  description: "",
+  summary: "",
+  tags: ["example"], // use openAPITags name - Example: openAPITags.category.name
+  responses: {
+    200: {
+      description: "example retrieved successfully",
+      // content: {"application/json": {schema: exampleResponseSchema,},},
+    },
+  },
+});
+
+
+
+
+// TODO: Add your openAPI specification here
+//  Full Example 
+// registry.registerPath({
+//   method: "get",
+//   path: "/api/example", // use openAPITags basepath - Example: openAPITags.category.basepath
+//   description: "",
+//   summary: "",
+//   tags: ["example"], // use openAPITags name - Example: openAPITags.category.name
+//   responses: {
+//     200: {
+//       description: "example retrieved successfully",
+//       content: {
+//         "application/json": {
+//           schema: exampleResponseSchema,
+//         },
+//       },
+//     },
+//     500: {
+//       description: "Internal server error",
+//       content: {
+//         "application/json": {
+//           schema: ErrorResponseSchema,
+//         },
+//       },
+//     },
+//   },
+// });
+
+
